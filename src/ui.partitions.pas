f7c754fdf41e7f3db2ce84222630cd9a48bab66d
Kagamma <kagamma.km@gmail.com>
2025-01-23 09:06:48 +0000

    Minor

--- src/ui.partitions.pas
+++ src/ui.partitions.pas
@@ -237,7 +237,9 @@ begin
       IsMountedSymbol := 'M '
     else
     if PPart^.Number < 0 then
-      IsMountedSymbol := '? ';
+      IsMountedSymbol := '* '
+    else
+      IsMountedSymbol := '  ';
     S := Format('%s│%s│%s│%s│%s│%s│%s', [
       PadRightLimit(IsMountedSymbol + ExtractFileName(PPart^.GetPartitionPathForDisplay), 17),
       PadRightLimit(PPart^.FileSystem, 10),
