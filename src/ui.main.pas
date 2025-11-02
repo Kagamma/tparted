7d3d7af9ce41ed7226f9235e55f2a8efc4ee6588
Kagamma <kagamma.km@gmail.com>
2025-11-02 11:57:56 +0000

    Reverted the exit keycode from CtrlC to AltX. Need to figure out the way to unblock SIGINT

--- src/ui.main.pas
+++ src/ui.main.pas
@@ -190,7 +190,7 @@ begin
     NewItem(S_MenuLogs.ToUnicode, '', kbNoKey, cmMenuDisplayLog, hcNoContext,
     NewItem(S_MenuFileSystemSupport.ToUnicode, '', kbNoKey, cmMenuDisplayFileSystemSupport, hcNoContext,
     NewLine(
-    NewItem(S_Quit.ToUnicode, 'Ctrl-C', kbCtrlC, cmQuit, hcNoContext, nil))))))
+    NewItem(S_Quit.ToUnicode, 'Alt-X', kbAltX, cmQuit, hcNoContext, nil))))))
   ), FMenuDevices);
 
   M := NewMenu(Self.FMenuSystem);
